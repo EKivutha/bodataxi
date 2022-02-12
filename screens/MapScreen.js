45a93231d24493f7b279d87165298d94cb37e736
ezz <gerkibz@outlook.com>
2022-02-12 19:05:42 +0300

    added maps

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import MapView from 'react-native-maps';
 
-const MapScreen = () => {
+
+class MapScreen extends React.Component{
+    getInitialState(){
+        return {
+          region: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          },
+        };
+      }
+      
+      onRegionChange(region) {
+        this.setState({ region });
+      }
+    render(){
     return (
-        <View>
-            <Text>Map</Text>
-        </View>
-    )
-}
+        <MapView
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+      />
+    );
+}}
 
 export default MapScreen
 
