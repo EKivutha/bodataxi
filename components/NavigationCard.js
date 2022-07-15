5980e0004fbce43ce8c983ff4fe7fb226cc5aac5
Ezra <gerkibz@outlook.com>
2022-07-15 19:19:54 +0300

    add navigation card component

---
+++ components/NavigationCard.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function NavigationCard() {
+  return (
+    <SafeAreaView>
+      <Text>NavigationCard</Text>
+      
+    </SafeAreaView>
+  )
+}
