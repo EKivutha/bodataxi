5d6d61d5b0ebe7fba08121540def8daf2a43e08e
Ezra <gerkibz@outlook.com>
2022-07-15 19:20:22 +0300

    add ride options component

---
+++ components/RideOptions.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RideOptions() {
+  return (
+    <View>
+      <Text>RideOptions</Text>
+    </View>
+  )
+}
