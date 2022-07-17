7016da323c7599ae9e08de870db3a78cc2e789ac
Ezra <gerkibz@outlook.com>
2022-07-17 21:21:26 +0300

    update tailwind dependancy

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { FlatList } from 'react-native-gesture-handler'
 import { Icon } from 'react-native-vector-icons/Icon'
+import tw from 'twrnc';
 
 data = [
     {
